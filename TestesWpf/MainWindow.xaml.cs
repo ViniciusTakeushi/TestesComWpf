109a63816eeb3fb6251d80fe5d4e333acf8ad6e0
Vinicius Kendi Takeushi <vinicius.takeushi@outlook.com>
2018-08-24 02:29:57 -0300

    Eventos nos elementos e montagem do layout

--- TestesWpf/MainWindow.xaml.cs
+++ TestesWpf/MainWindow.xaml.cs
@@ -24,5 +24,42 @@ namespace TestesWpf
         {
             InitializeComponent();
         }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ok");
+
+            if (this.c1.IsChecked == true)
+                MessageBox.Show("ok1");
+            else
+                MessageBox.Show("ok2");
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var cmb = (ComboBox)sender;
+
+            var itemSelected = ((ComboBoxItem)cmb.SelectedItem).Content;
+
+            MessageBox.Show(itemSelected.ToString());
+        }
+
+        private void c3_Checked(object sender, RoutedEventArgs e)
+        {
+            var check = (CheckBox)sender;
+
+            if (check.IsChecked == true)
+                MessageBox.Show("check");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("init");
+        }
+
+        private void txtDescription_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblTest.Text = txtDescription.Text;
+        }
     }
 }
